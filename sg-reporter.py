599d182d0f2a3f7008b6a397ad4946655fc461f8
Guido de Giovanni <guidito.dg86@gmail.com>
2023-09-03 14:21:27 +0100

    First version of reporter

--- sg-reporter.py
+++ sg-reporter.py
@@ -1,7 +1,14 @@
 import boto3
 from botocore.exceptions import ClientError
+import requests
+import os
+
+
+client = boto3.client("sts")
+account_id = client.get_caller_identity()["Account"]
 
 ec2 = boto3.client('ec2')
+
 
 try:
     sec_group_data = ec2.describe_security_groups()
@@ -38,3 +45,64 @@ for item in ec2_instances['Reservations']:
         ec2_p[name]['ip_addr']=ip_addr
         ec2_p[name]['sgs_applied']=sgs_applied     
 pass
+
+
+headers_netbox = {
+    'Authorization': f'Token {os.getenv("TF_VAR_netbox_token")}',
+}
+prefix_url = "http://127.0.0.1:8000/api/ipam/prefixes/?q="
+prefix_ip_addr = "http://127.0.0.1:8000/api/ipam/prefixes/?contains="
+
+
+results=[]
+for sec_group in sec_groups_p:
+    sec_id = sec_groups_p[sec_group]["id"]
+    for ingress_statement in sec_groups_p[sec_group]["ingress"]:
+        if len(ingress_statement["IpRanges"]) == 0:
+            #put here security groups as source
+            pass
+        protocol=ingress_statement['IpProtocol']
+        if ingress_statement['FromPort']==ingress_statement['ToPort']:
+            ports_opened = ingress_statement['FromPort']
+        else:
+            ports_opened = str(ingress_statement['FromPort']) + "-" + str(ingress_statement['ToPort'])
+        for ip_range in ingress_statement['IpRanges']:
+            subnet = ip_range['CidrIp']
+            if subnet == "0.0.0.0/0":
+                site = "sa"
+                role = "any"
+            else:
+                target_url = prefix_url + subnet
+                prefix_data =  requests.get(target_url , headers=headers_netbox).json()
+                site = prefix_data['results'][0]['site']['name']
+                role = prefix_data['results'][0]['role']['name']
+            for ec2_instance in ec2_p:
+                for item in ec2_p[ec2_instance]['sgs_applied']:
+                    if item['GroupId'] == sec_id:
+                        ec2_ip_addr = ec2_p[ec2_instance]['ip_addr']
+                        ec2_id = ec2_p[ec2_instance]['id']
+                        target_url = prefix_ip_addr + ec2_ip_addr 
+                        ip_data =  requests.get(target_url , headers=headers_netbox).json()
+                        dst_subnet = ip_data['results'][0]['prefix']
+                        site_dst = ip_data['results'][0]['site']['name']
+                        role_dst = ip_data['results'][0]['role']['name']
+                        pass
+
+                        results.append(
+                            [
+                                account_id,
+                                sec_id,
+                                subnet,
+                                f"R-{role}-{site}",
+                                ec2_instance,
+                                ec2_id,
+                                ec2_ip_addr,
+                                dst_subnet,
+                                f"R-{role_dst}-{site_dst}",
+                                f"{protocol}={ports_opened}"
+                            ]
+                        )
+        
+pass
+
+
