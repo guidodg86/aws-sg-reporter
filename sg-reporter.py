1c508e21e0c284182b5b2bd8983785d6ebdccd88
Guido de Giovanni <guidito.dg86@gmail.com>
2023-09-04 08:17:06 +0100

    adding security group as source

--- sg-reporter.py
+++ sg-reporter.py
@@ -58,14 +58,56 @@ results=[]
 for sec_group in sec_groups_p:
     sec_id = sec_groups_p[sec_group]["id"]
     for ingress_statement in sec_groups_p[sec_group]["ingress"]:
-        if len(ingress_statement["IpRanges"]) == 0:
-            #put here security groups as source
-            pass
         protocol=ingress_statement['IpProtocol']
         if ingress_statement['FromPort']==ingress_statement['ToPort']:
             ports_opened = ingress_statement['FromPort']
         else:
             ports_opened = str(ingress_statement['FromPort']) + "-" + str(ingress_statement['ToPort'])
+        if len(ingress_statement["IpRanges"]) == 0:
+            draft_results = []
+            for ec2_instance in ec2_p:
+                for item in ec2_p[ec2_instance]['sgs_applied']:
+                    if item['GroupId'] == sec_id:
+                        ec2_ip_addr = ec2_p[ec2_instance]['ip_addr']
+                        ec2_id = ec2_p[ec2_instance]['id']
+                        target_url = prefix_ip_addr + ec2_ip_addr 
+                        ip_data =  requests.get(target_url , headers=headers_netbox).json()
+                        dst_subnet = ip_data['results'][0]['prefix']
+                        site_dst = ip_data['results'][0]['site']['name']
+                        role_dst = ip_data['results'][0]['role']['name']
+                        draft_results.append(
+                            [
+                                ec2_instance,
+                                ec2_id,
+                                ec2_ip_addr,
+                                dst_subnet,
+                                f"R-{role_dst}-{site_dst}",
+                                f"{protocol}={ports_opened}"
+                            ]
+                        )
+                        ec2_source_list = {}
+                        for item in draft_results:
+                            ec2_source_list[item[1]] = item[0]
+                        for item in draft_results:
+                            src_string = ""
+                            for instance in ec2_source_list:
+                                if instance == item[1]:
+                                    continue
+                                src_string = src_string + ";" + ec2_source_list[instance] + "__" + instance
+                            results.append(
+                                [
+                                    account_id,
+                                    sec_id,
+                                    "security_group as source",
+                                    src_string[1:],
+                                    item[0],
+                                    item[1],
+                                    item[2],
+                                    item[3],
+                                    item[4],
+                                    item[5]
+                                ]
+                            )                            
         for ip_range in ingress_statement['IpRanges']:
             subnet = ip_range['CidrIp']
             if subnet == "0.0.0.0/0":
@@ -86,8 +128,6 @@ for sec_group in sec_groups_p:
                         dst_subnet = ip_data['results'][0]['prefix']
                         site_dst = ip_data['results'][0]['site']['name']
                         role_dst = ip_data['results'][0]['role']['name']
-                        pass
-
                         results.append(
                             [
                                 account_id,
